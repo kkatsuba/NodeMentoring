df6d833b7d5abcd5ab60cc087c74b83162ee73d8
Kiryl Katsuba <katsuba1995@gmail.com>
2017-10-14 16:41:48 +0300

    Init task 3

---
+++ app.js
@@ -0,0 +1,1 @@
+const streams = require('./utils/streams');
---
+++ utils/streams.js
@@ -0,0 +1,25 @@
+const argv = require('minimist')(process.argv.slice(2));
+
+const inputOutput = (filePath) => {
+
+};
+
+const transformFile = (filePath) => {
+
+};
+
+const transform = () => {
+
+};
+
+const httpClient = () => {
+
+};
+
+const httpServer = () => {
+
+};
+
+const printHelpMessage = () => {
+
+};
