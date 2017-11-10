ae3f8f3fa9138487481dd826a1c8a54c7fc8d1c4
Kiryl Katsuba <katsuba1995@gmail.com>
2017-11-10 18:18:41 +0300

    Some amazing.. hmmm.. nothing

--- mentoring.sql
+++ mentoring.sql
@@ -18,10 +18,10 @@ CREATE TABLE IF NOT EXISTS public.products
     CONSTRAINT products_pkey PRIMARY KEY (id)
 );
 
-INSERT INTO users (1, 'abba', 'terminator', 'abbter', 'pteradaktel@haha.com');
-INSERT INTO users (2, 'abba1', 'terminator1', 'abbter1', 'pteradaktel1@haha.com');
-INSERT INTO users (3, 'abba2', 'terminator2', 'abbter2', 'pteradaktel2@haha.com');
-INSERT INTO users (4, 'abba3', 'terminator3', 'abbter3', 'pteradaktel3@haha.com');
+INSERT INTO users VALUES (1, 'abba', 'terminator', 'abbter', 'pteradaktel@haha.com');
+INSERT INTO users VALUES (2, 'abba1', 'terminator1', 'abbter1', 'pteradaktel1@haha.com');
+INSERT INTO users VALUES (3, 'abba2', 'terminator2', 'abbter2', 'pteradaktel2@haha.com');
+INSERT INTO users VALUES (4, 'abba3', 'terminator3', 'abbter3', 'pteradaktel3@haha.com');
 
-INSERT INTO products (1, 'just_product_1', 'opdoapsjdaodjopasjdoapsdjpajsdpasdajsdpasjdpoasdjpa');
-INSERT INTO products (2, 'just_product_2', 'ojiojqweiqowejqwjeoqweiwieiqiweiieieieieieieieeiieie');
+INSERT INTO products VALUES (1, 'just_product_1', 'opdoapsjdaodjopasjdoapsdjpajsdpasdajsdpasjdpoasdjpa');
+INSERT INTO products VALUES (2, 'just_product_2', 'ojiojqweiqowejqwjeoqweiwieiqiweiieieieieieieieeiieie');
